ae864e0263d36f319cc12c3885695b0549d55163
Niko Köbler <niko@n-k.de>
2016-02-27 21:00:54 +0100

    remove unnecessary content for pages

--- create.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-mkdir -p $HOME/pages
-mkdir -p $HOME/pages/images
-BASEDIR=$(dirname $0)
-cd $BASEDIR
-cp ./images/*.* $HOME/pages/images/
-asciidoctor index.adoc -d book -D $HOME/pages
